066e6f014ad2f0a0e2295a9bdf04cdd85aa85b7a
Laney <laneyobermueller@gmail.com>
2023-11-05 20:21:45 -0500

    create calendar base and add event button

--- app/src/main/java/com/example/calender/MainActivity.kt
+++ app/src/main/java/com/example/calender/MainActivity.kt
@@ -1,11 +1,68 @@
-package com.example.calender
+package com.example.calender;
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.content.Intent
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.icu.text.SimpleDateFormat
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Button
+import android.widget.CalendarView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.calender.R
 
 class MainActivity : AppCompatActivity() {
+    // on below line we are creating
+    // variables for text view and calendar view
+    lateinit var dateTV: TextView
+    lateinit var calendarView: CalendarView
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Declaring and initializing
+        // the button from the layout file
+        val mButton = findViewById<Button>(R.id.button)
+
+        // Registering ID's
+        val textView = findViewById<TextView>(R.id.idTVDate)
+        val calenderView = findViewById<CalendarView>(R.id.calendarView)
+
+        // using setonDateChangeListener
+        calenderView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val date = "$month/$dayOfMonth/$year"
+
+            // changing the textview
+            // data to selected date
+            textView.text = date
+        }
+
+        // Event start and end time with date
+        val startTime = "2022-02-1T09:00:00"
+        val endTime = "2022-02-1T12:00:00"
+
+        // Parsing the date and time
+        val mSimpleDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        val mStartTime = mSimpleDateFormat.parse(startTime)
+        val mEndTime = mSimpleDateFormat.parse(endTime)
+
+        // When Button is clicked, Intent started
+        // to create an event with given time
+        mButton.setOnClickListener {
+            val mIntent = Intent(Intent.ACTION_EDIT)
+            mIntent.type = "vnd.android.cursor.item/event"
+            mIntent.putExtra("beginTime", mStartTime.time)
+            mIntent.putExtra("time", true)
+            mIntent.putExtra("rule", "FREQ=YEARLY")
+            mIntent.putExtra("endTime", mEndTime.time)
+            mIntent.putExtra("title", "Geeksforgeeks Event")
+            startActivity(mIntent)
+        }
     }
 }
